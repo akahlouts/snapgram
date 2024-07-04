adf0d56af8c5119b1572a51d153a1b60c6373efc
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-07-04 19:24:24 +0300

    Building Auth Pages

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const SignupValidation = z.object({
+  name: z.string().min(2, { message: "To short" }),
+  username: z.string().min(2, { message: "To short" }),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at leat 8 characters." }),
+});
